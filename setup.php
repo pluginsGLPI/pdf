e51b9ec42719d1f3119f756a4b989423f353895b
RomainLvr <lecouvreurromain@gmail.com>
2024-12-20 15:08:54 +0100

    Fix file headers

--- setup.php
+++ setup.php
@@ -1,43 +1,36 @@
 <?php
 
 /**
- * ---------------------------------------------------------------------
+ *  -------------------------------------------------------------------------
+ *  LICENSE
  *
- * GLPI - Gestionnaire Libre de Parc Informatique
+ *  This file is part of PDF plugin for GLPI.
  *
- * http://glpi-project.org
+ *  PDF is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PDF is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with Reports. If not, see <http://www.gnu.org/licenses/>.
  *
  * @author    Nelly Mahu-Lasson, Remi Collet, Teclib
+ * @copyright Copyright (c) 2009-2022 PDF plugin team
  * @copyright 2015-2024 Teclib' and contributors.
  * @copyright 2003-2014 by the INDEPNET Development Team.
- * @copyright Copyright (c) 2009-2022 PDF plugin team
  * @licence   https://www.gnu.org/licenses/gpl-3.0.html
  * @license   AGPL License 3.0 or (at your option) any later version
  * @link      https://github.com/pluginsGLPI/pdf/
  * @link      http://www.glpi-project.org/
  * @package   pdf
  * @since     2009
- *
- * ---------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of GLPI.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * ---------------------------------------------------------------------
+ *             http://www.gnu.org/licenses/agpl-3.0-standalone.html
+ *  --------------------------------------------------------------------------
  */
 
 function plugin_init_pdf()
